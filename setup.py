3196b92305a65169e059e9504e7373606c828dcc
Yashu Seth <yashuseth2503@gmail.com>
2018-04-05 06:07:00 +0530

    changed directory structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dummyPy',
+  packages = ['dummyPy'],
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Yashu Seth',
+  author_email = 'yashuseth2503@gmail.com',
+  url = 'https://github.com/yashu-seth/dummyPy',
+  download_url = 'https://github.com/yashu-seth/dummyPy/archive/0.1.tar.gz',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
